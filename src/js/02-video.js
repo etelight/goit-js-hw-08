ea92432a9f51ba1954332229e583abb6eb70b419
etelight <etelight@gmail.com>
2023-06-20 12:59:43 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoEl = document.getElementById('vimeo-player');
+const player = new Player(videoEl);
+
+const throttledTimeUpdate = throttle(seconds => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  throttledTimeUpdate(data.seconds);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
